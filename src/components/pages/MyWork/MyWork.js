e17a65e0a43935d59dbf3c7a70af0be6a3833d2f
Ja-boop <agusmisiones@gmail.com>
2021-07-22 18:54:46 -0300

    add

--- src/components/pages/MyWork/MyWork.js
+++ src/components/pages/MyWork/MyWork.js
@@ -2,36 +2,43 @@ import React from 'react';
 import './MyWork.css'
 import '../../css/cybrTheme.css'
 import rentCarImg from './img/rent-a-car.png'
+import nothingHereImg from './img/rent-a-car.png'
 
 const MyWork = () => {
     return (
-        <div className="box my-work-box wrapper">
-            <div className="grid-1">
+        <div className="my-work-box wrapper">
+            <div className="grid-1 grid-is-centered">
                 <img src={rentCarImg} alt="rent car" className="img-grid"></img>
+                <div className="project-links-1 links-overlay">
+                    <a href="https://www.w3schools.com" target="_blank" rel="noreferrer" className="project-links-style" title="DEMO">DEMO</a>
+                </div>
+                <div className="project-links-2 links-overlay">
+                    <a href="https://github.com/Ja-boop/rent-a-car" target="_blank" rel="noreferrer" className="project-links-style" title="GITHUB">GITHUB</a>
+                </div>
             </div>
-            <div className="grid-2">
-            <img src={rentCarImg} alt="rent car" className="img-grid"></img>
+            <div className="grid-2 grid-is-centered">
+                <h1 className="nothing-here">Nothing Here</h1>
             </div>
-            <div className="grid-3">
-            <img src={rentCarImg} alt="rent car" className="img-grid"></img>
+            <div className="grid-3 grid-is-centered">
+                <h1 className="nothing-here">Nothing Here</h1>
             </div>
-            <div className="grid-4">
-            <img src={rentCarImg} alt="rent car" className="img-grid"></img>
+            <div className="grid-4 grid-is-centered">
+                <h1 className="nothing-here">Nothing Here</h1>
             </div>
-            <div className="grid-5">
-            <img src={rentCarImg} alt="rent car" className="img-grid"></img>
+            <div className="grid-5 grid-is-centered">
+                <h1 className="nothing-here">Nothing Here</h1>
             </div>
-            <div className="grid-6">
-            <img src={rentCarImg} alt="rent car" className="img-grid"></img>
+            <div className="grid-6 grid-is-centered">
+                <h1 className="nothing-here">Nothing Here</h1>
             </div>
-            <div className="grid-7">
-            <img src={rentCarImg} alt="rent car" className="img-grid"></img>
+            <div className="grid-7 grid-is-centered">
+                <h1 className="nothing-here">Nothing Here</h1>
             </div>
-            <div className="grid-8">
-            <img src={rentCarImg} alt="rent car" className="img-grid"></img>
+            <div className="grid-8 grid-is-centered">
+                <h1 className="nothing-here">Nothing Here</h1>
             </div>
-            <div className="grid-9">
-            <img src={rentCarImg} alt="rent car" className="img-grid"></img>
+            <div className="grid-9 grid-is-centered">
+                <h1 className="nothing-here">Nothing Here</h1>
             </div>
         </div>
     )
