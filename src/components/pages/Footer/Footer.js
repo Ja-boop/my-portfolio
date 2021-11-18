13091583af9d773313cab441b73ed04343c19143
Ja-boop <agusmisiones@gmail.com>
2021-11-18 00:03:47 -0300

    set links

--- src/components/pages/Footer/Footer.js
+++ src/components/pages/Footer/Footer.js
@@ -15,14 +15,14 @@ const Footer = () => {
                 <p>¡Muchas gracias!</p>
             </div>
             <div className="footer-links">
-                <h2 className="cyberpunk">Linkedin</h2>
-                <h2 className="cyberpunk">Github</h2>
-                <h2 className="cyberpunk">Email</h2>
+                <h2 className="cyberpunk"><a href="https://www.linkedin.com/in/viktor-agust%C3%ADn-luzny-41529617a/">Linkedin</a></h2>
+                <h2 className="cyberpunk"><a href="https://github.com/Ja-boop">GitHub</a></h2>
+                <h2 className="cyberpunk"><a href="mailto:viktor.luzny@gmail.com">Email</a></h2>
             </div>
             <div className="footer-icons">  
-                <FontAwesomeIcon icon={faLinkedin} color="white" size="5x" />
-                <FontAwesomeIcon icon={faGithub} color="white" size="5x" />
-                <FontAwesomeIcon icon={faEnvelopeSquare} color="white" size="5x" />
+                <FontAwesomeIcon icon={faLinkedin} color="white" size="5x" href="https://www.linkedin.com/in/viktor-agust%C3%ADn-luzny-41529617a/" />
+                <FontAwesomeIcon icon={faGithub} color="white" size="5x" href="https://github.com/Ja-boop" />
+                <FontAwesomeIcon icon={faEnvelopeSquare} color="white" size="5x" href="mailto:viktor.luzny@gmail.com" />
             </div>
         </footer>
     )
